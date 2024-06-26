ea54b704f899cd73a74bb68ef398b5e71de1dbc4
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-06-26 22:30:52 +0400

    developed

--- src/main/java/com/example/securityjwt/SecurityJwtApplication.java
+++ src/main/java/com/example/securityjwt/SecurityJwtApplication.java
@@ -1,12 +1,36 @@
 package com.example.securityjwt;
 
+import com.example.securityjwt.entity.User;
+import com.example.securityjwt.repository.UserRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 @SpringBootApplication
+@RequiredArgsConstructor
 public class SecurityJwtApplication {
 
+	private final UserRepository userRepository;
+
+	@PostConstruct
+	public void createUsers() {
+		List<User> users = Stream.of(
+				new User(1,"safar","12345","safar@gmailcom"),
+				new User(1,"tunzala","12345","tunzala@gmailcom"),
+				new User(1,"farac","12345","farac@gmailcom"),
+				new User(1,"rovnaq","12345","rovnaq@gmailcom")
+		).collect(Collectors.toList());
+
+		userRepository.saveAll(users);
+	}
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(SecurityJwtApplication.class, args);
 	}
 
