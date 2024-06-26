09971526ef146b2a9a6eb996e1df6b854a3ad161
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-06-26 21:58:59 +0400

    developed

--- src/main/java/com/example/securityjwt/service/JwtService.java
+++ src/main/java/com/example/securityjwt/service/JwtService.java
@@ -4,6 +4,9 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
@@ -17,5 +20,24 @@ public class JwtService {
 
     public <T> T extractClaim(String token, Function<Claims,T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
     }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token,Claims::getExpiration);
+    }
+    public String extractUsername(String token) {
+        return extractClaim(token,Claims::getSubject);
+    }
+
+    public Boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    public String generateToken(String username) {
+        Map<String,Object> claims = new HashMap<>();
+        return createToken(claims,username);
+    }
+
+
 }
