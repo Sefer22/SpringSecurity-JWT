3115cbf6b144dd24c91762da88fb1f2cb5b76f3c
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-06-26 21:29:16 +0400

    developed

--- src/main/java/com/example/securityjwt/entity/User.java
+++ src/main/java/com/example/securityjwt/entity/User.java
@@ -1,6 +1,7 @@
 package com.example.securityjwt.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,4 +13,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Table(name = "users")
 public class User {
+
+    @Id
+    private Integer id;
+    private String username;
+    private String password;
+    private String email;
 }
