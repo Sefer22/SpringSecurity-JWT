5c2adb1fe40b8f11cf59db7349f083d2c864a598
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-06-26 21:50:11 +0400

    developed

--- src/main/java/com/example/securityjwt/service/JwtService.java
+++ src/main/java/com/example/securityjwt/service/JwtService.java
@@ -4,6 +4,8 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Service;
 
+import java.util.function.Function;
+
 @Service
 public class JwtService {
 
@@ -11,5 +13,9 @@ public class JwtService {
 
     private Claims extractAllClaims(String token) {
         return Jwts.parser().setSigningKey(SECRET_KEY).build().parseSignedClaims(token).getBody();
+    }
+
+    public <T> T extractClaim(String token, Function<Claims,T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
     }
 }
