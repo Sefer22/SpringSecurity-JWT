6f507e33cec25750bb480369481faf1c00d277f1
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-06-27 15:04:57 +0400

    developed

--- src/main/java/com/example/securityjwt/filter/JwtFilter.java
+++ src/main/java/com/example/securityjwt/filter/JwtFilter.java
@@ -4,13 +4,22 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Component
 public class JwtFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authorizationHeader = request.getHeader("Authorization");
 
+        String token=null;
+        String username = null;
+
+        if(authorizationHeader !=null && authorizationHeader.startsWith("Bearer ")) {
+
+        }
     }
 }
