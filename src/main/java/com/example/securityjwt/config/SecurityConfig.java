eafd11984a19239e2a184b1494acf67cb61b012c
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-06-27 14:41:28 +0400

    developed

--- src/main/java/com/example/securityjwt/config/SecurityConfig.java
+++ src/main/java/com/example/securityjwt/config/SecurityConfig.java
@@ -1,9 +1,39 @@
 package com.example.securityjwt.config;
 
+import com.example.securityjwt.service.UserDetailsServiceImpl;
+import jakarta.servlet.Filter;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailsServiceImpl userDetailsService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+       http
+               .csrf(csrf-> csrf.disable())
+               .authorizeRequests().requestMatchers("/authenticate").permitAll()
+               .anyRequest().authenticated()
+               .and()
+               .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+
 }
