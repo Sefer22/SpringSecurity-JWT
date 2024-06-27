ae9303577829eaf1d4042ca7080ec9e878f746ed
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-06-27 14:25:21 +0400

    developed

--- src/main/java/com/example/securityjwt/controller/HomeController.java
+++ src/main/java/com/example/securityjwt/controller/HomeController.java
@@ -5,6 +5,7 @@ import com.example.securityjwt.service.JwtService;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,7 +26,14 @@ public class HomeController {
     @PostMapping("/authenticate")
     public String generateToken(@RequestBody AuthRequest authRequest) throws Exception {
         try{
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authRequest.getUsername(),authRequest.getPassword())
+            );
 
-        }catch ()
+        }catch (Exception exception) {
+            throw new Exception("Invalid username or password !");
+        }
+
+        return jwtService.generateToken(authRequest.getUsername());
     }
 }
